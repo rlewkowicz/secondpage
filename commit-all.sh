5f793e6f0543df3972e775b77896e19a38d95b6d
ryan.lewkowicz@ge.com <ryan.lewkowicz@ge.com>
2018-06-07 10:30:18 -0500

    auto commit message

--- commit-all.sh
+++ commit-all.sh
@@ -4,8 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 (cd $DIR && git add -A && git commit -m "auto commit message" && git push ) &
 
-(cd $DIR/secondpage-pythonutils && git add -A && git commit -m "auto commit message" && git push ) &
-(cd $DIR/secondpage-api && git add -A && git commit -m "auto commit message" && git push ) &
-(cd $DIR/secondpage-render && git add -A && git commit -m "auto commit message" && git push ) &
-(cd $DIR/secondpage-frontend && git add -A && git commit -m "auto commit message" && git push ) &
-(cd $DIR/secondpage-rssparse && git add -A && git commit -m "auto commit message" && git push ) &
+(cd $DIR/secondpage-pythonutils && git add -A && git commit -m "auto commit message"; git push ) &
+(cd $DIR/secondpage-api && git add -A && git commit -m "auto commit message"; git push ) &
+(cd $DIR/secondpage-render && git add -A && git commit -m "auto commit message"; git push ) &
+(cd $DIR/secondpage-frontend && git add -A && git commit -m "auto commit message"; git push ) &
+(cd $DIR/secondpage-rssparse && git add -A && git commit -m "auto commit message"; git push ) &
